e95104d0b4a74156f52bb0b91c13d8ccec0b716d
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-05-06 14:27:21 -0500

    added basic functionality for registering and logging in users. Also hashed passwords

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,14 +1,57 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
 
 
 
+const loginUser = async (req, res) => {
+   const { email, password } = req.body;
 
-const loginUser = (req, res) => {
-    res.send('Login');
+   try{
+        const user = await User.findOne({ email });
+
+        if(user && (await bcrypt.compare(password, user.password))){
+            res.status(200).send('Logged In');
+        } else{
+            res.status(401);
+            throw new Error('Invalid credentials')
+        }
+   }catch(err){
+       res.send(err.message);
+   }
 };
 
 
-const registerUser = (req, res) => {
-    res.send('Register');
+const registerUser = async (req, res) => {
+    const { name, email, password, confirmPassword } = req.body;
+
+    if(password !== confirmPassword) return res.send('Passwords do not match');
+
+    try{
+
+        const existingUser = await User.findOne({email});
+
+         // maybe sent an email about why account was not created
+        if(existingUser) {
+            res.status(400);
+            throw new Error('User already exists');
+        }
+
+        // Hash password
+        const salt = await bcrypt.genSalt(12);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        if(newUser) res.status(201).send('User created');
+
+    }catch(error){
+        console.log(error);
+        res.send(error)
+    }
 };
 
 module.exports = {
--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -5,5 +5,6 @@ const users = require('../routes/userRoutes');
 
 module.exports = function(app){
     app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
     app.use('/users', users);
 }
