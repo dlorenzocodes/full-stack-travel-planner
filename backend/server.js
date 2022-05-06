f2d4a613ee459304e586de20b75cf3b904228cdf
Donna Lorenzo <dlorenzo24@hotmail.com>
2022-05-06 10:46:39 -0500

    initial set-up

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+require('./startup/routes')(app);
+
+
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
